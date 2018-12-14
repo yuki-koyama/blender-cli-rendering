588665325a68ecba05fe257bb03e62719da9ac33
Yuki Koyama <yuki@koyama.xyz>
2018-12-15 00:07:03 +0900

    Add a shell script to run all the python scripts

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+RESOLUTION=100
+SAMPLINGS=1000
+
+mkdir -p out
+blender --background --python ./01_cube.py -- ./out/01_cube.png ${RESOLUTION}
+blender --background --python ./02_suzanne.py -- ./out/02_suzanne.png ${RESOLUTION}
+blender --background --python ./03_ibl.py -- ./out/03_ibl.png ${RESOLUTION}
+blender --background --python ./04_principled_bsdf.py -- ./out/04_principled_bsdf.png ${RESOLUTION} ${SAMPLINGS}
+blender --background --python ./05_composition.py -- ./out/05_composition.png ${RESOLUTION} ${SAMPLINGS}
+blender --background --python ./06_vignette.py -- ./out/06_vignette.png ${RESOLUTION} ${SAMPLINGS}
+blender --background --python ./07_texturing.py -- ./out/07_texturing.png ${RESOLUTION} ${SAMPLINGS}
