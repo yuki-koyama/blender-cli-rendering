36debb3c4f9332796809707152681e5bbcf21a6e
Yuki Koyama <yuki@koyama.xyz>
2018-12-15 10:25:35 +0900

    Remove unused functions

--- 07_texturing.py
+++ 07_texturing.py
@@ -41,60 +41,6 @@ def apply_subdivision_surface(target, level):
 	bpy.context.object.modifiers["Subsurf"].levels = level
 	bpy.ops.object.modifier_apply(apply_as='DATA', modifier="Subsurf")
 
-def set_principled_node_as_rough_blue(principled_node):
-	principled_node.inputs['Base Color'].default_value = (0.1, 0.2, 0.6, 1.0)
-	principled_node.inputs['Subsurface'].default_value = 0.0
-	principled_node.inputs['Subsurface Color'].default_value = (0.9, 0.9, 0.9, 1.0)
-	principled_node.inputs['Subsurface Radius'].default_value = (1.0, 1.0, 1.0)
-	principled_node.inputs['Metallic'].default_value = 0.5
-	principled_node.inputs['Specular'].default_value = 0.5
-	principled_node.inputs['Specular Tint'].default_value = 0.0
-	principled_node.inputs['Roughness'].default_value = 0.9
-	principled_node.inputs['Anisotropic'].default_value = 0.0
-	principled_node.inputs['Anisotropic Rotation'].default_value = 0.0
-	principled_node.inputs['Sheen'].default_value = 0.0
-	principled_node.inputs['Sheen Tint'].default_value = 0.5
-	principled_node.inputs['Clearcoat'].default_value = 0.0
-	principled_node.inputs['Clearcoat Roughness'].default_value = 0.030
-	principled_node.inputs['IOR'].default_value = 1.450
-	principled_node.inputs['Transmission'].default_value = 0.0
-
-def set_principled_node_as_ceramic(principled_node):
-	principled_node.inputs['Base Color'].default_value = (0.8, 0.8, 0.8, 1.0)
-	principled_node.inputs['Subsurface'].default_value = 0.1
-	principled_node.inputs['Subsurface Color'].default_value = (0.9, 0.9, 0.9, 1.0)
-	principled_node.inputs['Subsurface Radius'].default_value = (1.0, 1.0, 1.0)
-	principled_node.inputs['Metallic'].default_value = 0.2
-	principled_node.inputs['Specular'].default_value = 0.5
-	principled_node.inputs['Specular Tint'].default_value = 0.0
-	principled_node.inputs['Roughness'].default_value = 0.0
-	principled_node.inputs['Anisotropic'].default_value = 0.0
-	principled_node.inputs['Anisotropic Rotation'].default_value = 0.0
-	principled_node.inputs['Sheen'].default_value = 0.0
-	principled_node.inputs['Sheen Tint'].default_value = 0.5
-	principled_node.inputs['Clearcoat'].default_value = 0.9
-	principled_node.inputs['Clearcoat Roughness'].default_value = 0.030
-	principled_node.inputs['IOR'].default_value = 1.450
-	principled_node.inputs['Transmission'].default_value = 0.0
-
-def set_principled_node_as_gold(principled_node):
-	principled_node.inputs['Base Color'].default_value = (1.00, 0.75, 0.35, 1.0)
-	principled_node.inputs['Subsurface'].default_value = 0.0
-	principled_node.inputs['Subsurface Color'].default_value = (0.3, 0.1, 0.1, 1.0)
-	principled_node.inputs['Subsurface Radius'].default_value = (1.0, 1.0, 1.0)
-	principled_node.inputs['Metallic'].default_value = 1.0
-	principled_node.inputs['Specular'].default_value = 0.5
-	principled_node.inputs['Specular Tint'].default_value = 0.0
-	principled_node.inputs['Roughness'].default_value = 0.0
-	principled_node.inputs['Anisotropic'].default_value = 0.0
-	principled_node.inputs['Anisotropic Rotation'].default_value = 0.0
-	principled_node.inputs['Sheen'].default_value = 0.0
-	principled_node.inputs['Sheen Tint'].default_value = 0.5
-	principled_node.inputs['Clearcoat'].default_value = 0.5
-	principled_node.inputs['Clearcoat Roughness'].default_value = 0.030
-	principled_node.inputs['IOR'].default_value = 1.450
-	principled_node.inputs['Transmission'].default_value = 0.0
-
 def create_texture_node(nodes, path, is_color_data):
 	# Instantiate a new texture image node
 	texture_node = nodes.new(type='ShaderNodeTexImage')
