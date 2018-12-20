0601f6d9609dfd2e9e30050a18ca284f222f757b
Yuki Koyama <yuki@koyama.xyz>
2018-12-20 09:43:10 +0900

    Fix scripts

--- run.sh
+++ run.sh
@@ -11,3 +11,4 @@ blender --background --python ./04_principled_bsdf.py -- ./out/04_principled_bsd
 blender --background --python ./05_composition.py -- ./out/05_composition.png ${RESOLUTION} ${SAMPLINGS}
 blender --background --python ./06_vignette.py -- ./out/06_vignette.png ${RESOLUTION} ${SAMPLINGS}
 blender --background --python ./07_texturing.py -- ./out/07_texturing.png ${RESOLUTION} ${SAMPLINGS}
+blender --background --python ./08_animation.py -- ./out/temp/frame ${RESOLUTION} ${SAMPLINGS} && ffmpeg -r 24 -i ./out/temp/frame%04d.png -pix_fmt yuv420p ./out/08_animation.mp4
