403ddbb28001b42934e43b6bd5751d41396d63a3
Yuki Koyama <yuki@koyama.xyz>
2019-01-03 17:28:18 +0900

    Add functions to generate renderable meshes from armature skeleton

--- 10_mocap.py
+++ 10_mocap.py
@@ -4,6 +4,7 @@
 import bpy
 import sys
 import math
+import mathutils
 import os
 
 sys.path.append(os.getcwd())
@@ -18,7 +19,109 @@ def set_smooth_shading(target_object):
 	for poly in target_object.data.polygons:
 		poly.use_smooth = True
 
-def create_skinned_object():
+def add_rigid_vertex_group(object, name, vertex_indices):
+	new_vertex_group = object.vertex_groups.new(name)
+	for vertex_index in vertex_indices:
+		new_vertex_group.add([ vertex_index ], 1.0, 'REPLACE')
+
+def generate_bone_mesh(radius, length):
+	base_radius = radius
+	top_radius = 0.5 * radius
+
+	vertices = [
+		mathutils.Vector((- base_radius, 0.0, + base_radius)),
+		mathutils.Vector((+ base_radius, 0.0, + base_radius)),
+		mathutils.Vector((+ base_radius, 0.0, - base_radius)),
+		mathutils.Vector((- base_radius, 0.0, - base_radius)),
+
+		mathutils.Vector((- top_radius, length, + top_radius)),
+		mathutils.Vector((+ top_radius, length, + top_radius)),
+		mathutils.Vector((+ top_radius, length, - top_radius)),
+		mathutils.Vector((- top_radius, length, - top_radius)),
+
+		mathutils.Vector((0.0, - base_radius, 0.0)),
+		mathutils.Vector((0.0, length + top_radius, 0.0))
+	]
+
+	faces = [
+		(8, 1, 0),
+		(8, 2, 1),
+		(8, 3, 2),
+		(8, 0, 3),
+
+		(9, 4, 5),
+		(9, 5, 6),
+		(9, 6, 7),
+		(9, 7, 4),
+
+		(0, 1, 5, 4),
+		(1, 2, 6, 5),
+		(2, 3, 7, 6),
+		(3, 0, 4, 7)
+	]
+
+	return vertices, faces
+
+def create_armature_mesh(scene, armature_object, mesh_name):
+	assert armature_object.type == 'ARMATURE', 'Error'
+	assert len(armature_object.data.bones) != 0, 'Error'
+
+	armature_data = armature_object.data
+
+	vertices = []
+	faces = []
+	vertex_groups = []
+
+	for bone in armature_data.bones:
+		radius = 0.10 * (0.10 + bone.length)
+		temp_vertices, temp_faces = generate_bone_mesh(radius, bone.length)
+
+		vertex_index_offset = len(vertices)
+
+		temp_vertex_group = { 'name': bone.name, 'vertex_indices': [] }
+		for local_index, vertex in enumerate(temp_vertices):
+			vertices.append(bone.matrix_local * vertex)
+			temp_vertex_group['vertex_indices'].append(local_index + vertex_index_offset)
+		vertex_groups.append(temp_vertex_group)
+
+		for face in temp_faces:
+			if len(face) == 3:
+				faces.append((face[0] + vertex_index_offset, face[1] + vertex_index_offset, face[2] + vertex_index_offset))
+			else:
+				faces.append((face[0] + vertex_index_offset, face[1] + vertex_index_offset, face[2] + vertex_index_offset, face[3] + vertex_index_offset))
+
+	# Add a new mesh and set vertices and faces
+	# In this case, it does not require to set edges
+	# After manipulating mesh data, update() needs to be called
+	new_mesh = bpy.data.meshes.new(mesh_name)
+	new_mesh.from_pydata(vertices, [], faces)
+	new_mesh.update()
+
+	new_object = bpy.data.objects.new(mesh_name, new_mesh)
+	new_object.matrix_world = armature_object.matrix_world
+
+	for vertex_group in vertex_groups:
+		add_rigid_vertex_group(new_object, vertex_group['name'], vertex_group['vertex_indices'])
+
+	armature_modifier = new_object.modifiers.new('Armature', 'ARMATURE')
+	armature_modifier.object = armature_object
+	armature_modifier.use_vertex_groups = True
+
+	utils.add_subdivision_surface_modifier(new_object, 1, is_simple=True)
+	utils.add_subdivision_surface_modifier(new_object, 2, is_simple=False)
+
+	scene.objects.link(new_object)
+
+	# Set the armature as the parent of the new object
+	bpy.ops.object.select_all(action='DESELECT')
+	new_object.select = True
+	armature_object.select = True
+	bpy.context.scene.objects.active = armature_object
+	bpy.ops.object.parent_set(type='OBJECT')
+
+	return new_object
+
+def create_armature_from_bvh(scene, bvh_path):
 	# Edit mode
 	bpy.ops.object.add(type='ARMATURE', enter_editmode=True, location=(0.0, 0.0, 0.0))
 	armature = bpy.context.object
@@ -48,39 +151,11 @@ def create_skinned_object():
 	# Object mode
 	bpy.ops.object.mode_set(mode='OBJECT')
 
-	# Mesh
-	bpy.ops.mesh.primitive_cube_add(location=(0.0, 0.0, 1.0), calc_uvs=True)
-	cube = bpy.context.object
-	cube.name = "Cuboid"
-	cube.scale = (0.5, 0.5, 1.0)
-	utils.add_subdivision_surface_modifier(cube, 3, is_simple=True)
-	utils.add_subdivision_surface_modifier(cube, 3, is_simple=False)
-	set_smooth_shading(cube)
-	mat = bpy.data.materials.new("Metal07")
-	mat.use_nodes = True
-	utils.clean_nodes(mat.node_tree.nodes)
-	utils.build_pbr_textured_nodes(
-		mat.node_tree, 
-		color_texture_path="./assets/textures/[2K]Metal07/Metal07_col.jpg", 
-		metallic_texture_path="./assets/textures/[2K]Metal07/Metal07_met.jpg", 
-		roughness_texture_path="./assets/textures/[2K]Metal07/Metal07_rgh.jpg", 
-		normal_texture_path="./assets/textures/[2K]Metal07/Metal07_nrm.jpg", 
-		displacement_texture_path="./assets/textures/[2K]Metal07/Metal07_disp.jpg"
-	)
-	cube.data.materials.append(mat)
-
-	# Set the armature as the parent of the cube using the "Automatic Weight" armature option
-	bpy.ops.object.select_all(action='DESELECT')
-	cube.select = True
-	armature.select = True
-	bpy.context.scene.objects.active = armature
-	bpy.ops.object.parent_set(type='ARMATURE_AUTO')
-
 	return armature
 
-def set_scene_objects():
-	current_object = create_skinned_object()
-	current_object.rotation_euler = (0.0, 0.0, math.pi * 60.0 / 180.0)
+def build_scene(scene):
+	armature = create_armature_from_bvh(scene, bvh_path='./assets/motion/131_03.bvh')
+	create_armature_mesh(scene, armature, 'Mesh')
 
 	bpy.ops.mesh.primitive_plane_add(radius=6.0, calc_uvs=True)
 	current_object = bpy.context.object
@@ -127,7 +202,7 @@ world = scene.world
 reset_scene()
 
 ## Suzannes
-focus_target = set_scene_objects()
+focus_target = build_scene(scene)
 
 ## Camera
 bpy.ops.object.camera_add(view_align=False, location=[0.0, - 12.0, 2.0])
