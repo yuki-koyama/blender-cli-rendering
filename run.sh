e43e60377a0dedac89f282f7d8e9f0b5b2bc9eae
Yuki Koyama <yuki@koyama.xyz>
2019-01-03 14:03:02 +0900

    Update shell script

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 RESOLUTION=100
-SAMPLINGS=1000
+SAMPLINGS=128
 
 mkdir -p out
 blender --background --python ./01_cube.py -- ./out/01_cube.png ${RESOLUTION}
@@ -11,4 +11,5 @@ blender --background --python ./04_principled_bsdf.py -- ./out/04_principled_bsd
 blender --background --python ./05_composition.py -- ./out/05_composition.png ${RESOLUTION} ${SAMPLINGS}
 blender --background --python ./06_vignette.py -- ./out/06_vignette.png ${RESOLUTION} ${SAMPLINGS}
 blender --background --python ./07_texturing.py -- ./out/07_texturing.png ${RESOLUTION} ${SAMPLINGS}
-blender --background --python ./08_animation.py -- ./out/temp/frame ${RESOLUTION} ${SAMPLINGS} && ffmpeg -r 24 -i ./out/temp/frame%04d.png -pix_fmt yuv420p ./out/08_animation.mp4
+blender --background --python ./08_animation.py -- ./out/08/frame ${RESOLUTION} ${SAMPLINGS} && ffmpeg -r 24 -i ./out/08/frame%04d.png -pix_fmt yuv420p ./out/08_animation.mp4
+blender --background --python ./09_armature.py -- ./out/09/frame ${RESOLUTION} ${SAMPLINGS} && ffmpeg -r 24 -i ./out/09/frame%04d.png -pix_fmt yuv420p ./out/09_armature.mp4
