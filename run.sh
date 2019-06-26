1a65aefd90f41e6dcd302a766ba43b1a8f4d20a9
Yuki Koyama <yuki@koyama.xyz>
2019-06-26 20:35:57 +0900

    Update the shell script

--- run.sh
+++ run.sh
@@ -15,3 +15,4 @@ blender --background --python ./08_animation.py -- ./out/08/frame ${RESOLUTION} 
 blender --background --python ./09_armature.py -- ./out/09/frame ${RESOLUTION} ${SAMPLINGS} && ffmpeg -r 24 -i ./out/09/frame%04d.png -pix_fmt yuv420p ./out/09_armature.mp4
 blender --background --python ./10_mocap.py -- ./assets/motion/102_01.bvh ./out/10/frame ${RESOLUTION} ${SAMPLINGS} && ffmpeg -r 24 -i ./out/10/frame%04d.png -pix_fmt yuv420p ./out/10_mocap.mp4
 blender --background --python ./11_mesh_visualization.py -- ./out/11_mesh_visualization.png ${RESOLUTION} ${SAMPLINGS}
+blender --background --python ./12_split_tone.py -- ./out/12_split_tone.png ${RESOLUTION} ${SAMPLINGS}
