ec0ac798409358b021e4b37fef19c1c750242f04
Yuki Koyama <yuki@koyama.xyz>
2019-10-10 11:15:00 +0900

    Add type hints

--- utils/lighting.py
+++ utils/lighting.py
@@ -1,18 +1,19 @@
 import bpy
+from typing import Tuple
 
 
-def create_area_light(location=(0.0, 0.0, 5.0),
-                      rotation=(0.0, 0.0, 0.0),
-                      size=5.0,
-                      color=(1.00, 0.90, 0.80, 1.00),
-                      strength=1000.0,
-                      name=None):
+def create_area_light(location: Tuple[float, float, float] = (0.0, 0.0, 5.0),
+                      rotation: Tuple[float, float, float] = (0.0, 0.0, 0.0),
+                      size: float = 5.0,
+                      color: Tuple[float, float, float, float] = (1.00, 0.90, 0.80, 1.00),
+                      strength: float = 1000.0,
+                      name: str = "") -> bpy.types.Object:
     if bpy.app.version >= (2, 80, 0):
         bpy.ops.object.light_add(type='AREA', location=location, rotation=rotation)
     else:
         bpy.ops.object.lamp_add(type='AREA', location=location, rotation=rotation)
 
-    if name is not None:
+    if name:
         bpy.context.object.name = name
 
     light = bpy.context.object.data
@@ -27,13 +28,15 @@ def create_area_light(location=(0.0, 0.0, 5.0),
     return bpy.context.object
 
 
-def create_sun_light(location=(0.0, 0.0, 5.0), rotation=(0.0, 0.0, 0.0), name=None):
+def create_sun_light(location: Tuple[float, float, float] = (0.0, 0.0, 5.0),
+                     rotation: Tuple[float, float, float] = (0.0, 0.0, 0.0),
+                     name: str = "") -> bpy.types.Object:
     if bpy.app.version >= (2, 80, 0):
         bpy.ops.object.light_add(type='SUN', location=location, rotation=rotation)
     else:
         bpy.ops.object.lamp_add(type='SUN', location=location, rotation=rotation)
 
-    if name is not None:
+    if name:
         bpy.context.object.name = name
 
     return bpy.context.object
