75c3469110a64acf7e479f1151e7b3fa1dfeafbe
Yuki Koyama <yuki@koyama.xyz>
2020-03-14 13:45:49 +0900

    Add some type hints

--- 02_suzanne.py
+++ 02_suzanne.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import utils
 
 
-def set_scene_objects():
+def set_scene_objects() -> bpy.types.Object:
     num_suzannes = 15
     for index in range(num_suzannes):
         utils.create_smooth_monkey(location=((index - (num_suzannes - 1) / 2) * 3.0, 0.0, 0.0),
--- 03_ibl.py
+++ 03_ibl.py
@@ -9,7 +9,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import utils
 
 
-def set_scene_objects():
+def set_scene_objects() -> bpy.types.Object:
     num_suzannes = 7
     for index in range(num_suzannes):
         utils.create_smooth_monkey(location=((index - (num_suzannes - 1) / 2) * 2.8, 0.0, 1.0),
