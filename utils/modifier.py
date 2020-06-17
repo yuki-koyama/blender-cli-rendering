830f577d542d0deb319d0b5effb7f5a1b810b97d
Yuki Koyama <yuki@koyama.xyz>
2020-06-17 18:24:49 +0900

    Fix a wrong type annotation

--- utils/modifier.py
+++ utils/modifier.py
@@ -1,8 +1,9 @@
 import bpy
 
 
-def add_subdivision_surface_modifier(mesh: bpy.types.Mesh, level: int, is_simple: bool = False) -> None:
-    modifier: bpy.types.SubsurfModifier = mesh.modifiers.new(name="Subsurf", type='SUBSURF')
+def add_subdivision_surface_modifier(mesh_object: bpy.types.Object, level: int, is_simple: bool = False) -> None:
+
+    modifier: bpy.types.SubsurfModifier = mesh_object.modifiers.new(name="Subsurf", type='SUBSURF')
 
     modifier.levels = level
     modifier.render_levels = level
