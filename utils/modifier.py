d8335ed21d7a0da1682d5e004058083622ccecdb
Yuki Koyama <yuki@koyama.xyz>
2020-06-17 18:25:31 +0900

    Add a utility function for solidify modifier

--- utils/modifier.py
+++ utils/modifier.py
@@ -8,3 +8,25 @@ def add_subdivision_surface_modifier(mesh_object: bpy.types.Object, level: int, 
     modifier.levels = level
     modifier.render_levels = level
     modifier.subdivision_type = 'SIMPLE' if is_simple else 'CATMULL_CLARK'
+
+
+def add_solidify_modifier(mesh_object: bpy.types.Object,
+                          thickness: float = 0.01,
+                          flip_normal: bool = False,
+                          fill_rim: bool = True,
+                          material_index_offset: int = 0,
+                          shell_vertex_group: str = "",
+                          rim_vertex_group: str = "") -> None:
+
+    modifier: bpy.types.SolidifyModifier = mesh_object.modifiers.new(name="Solidify", type='SOLIDIFY')
+
+    modifier.material_offset = material_index_offset
+    modifier.thickness = thickness
+    modifier.use_flip_normals = flip_normal
+    modifier.use_rim = fill_rim
+
+    # TODO: Check whether shell_vertex_group is either empty or defined
+    # TODO: Check whether rim_vertex_group is either empty or defined
+
+    modifier.shell_vertex_group = shell_vertex_group
+    modifier.rim_vertex_group = rim_vertex_group
