d6d320330d63ce4c6b8ca6aa8ce6d54bb5a08d22
Yuki Koyama <yuki@koyama.xyz>
2021-01-25 23:45:20 +0900

    Change the shape of the pixel data in numpy

--- utils/image.py
+++ utils/image.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def get_image_pixels_in_numpy(image: bpy.types.Image) -> np.array:
-    return np.array(image.pixels[:])
+    return np.array(image.pixels[:]).reshape(image.size[0] * image.size[1], image.channels)
 
 
 def set_image_pixels_in_numpy(image: bpy.types.Image, pixels: np.array) -> None:
